2f3583af5d2d2813d37748609f49a300a4d24c6e
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-03-26 00:04:00 +0300

    Initial commit

---
+++ Assets/1_Kinematic Rigidbody/Scripts/KinematicBall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace _1_Kinematic_Rigidbody.Scripts
+{
+	public class KinematicBall : MonoBehaviour
+	{
+		public Animator Animator;
+		public Rigidbody Rigidbody;
+
+		private void OnCollisionEnter(Collision other)
+		{
+			if (Rigidbody.isKinematic)
+			{
+				var lastMotion = Rigidbody.position - _lastPosition;
+				var velocity = lastMotion / Time.deltaTime;
+				Animator.enabled = false;
+				Rigidbody.isKinematic = false;
+				Rigidbody.velocity = velocity;
+			}
+		}
+
+		private void FixedUpdate()
+		{
+			_lastPosition = Rigidbody.position;
+		}
+
+		private Vector3 _lastPosition;
+	}
+}
